2dc19e4c3e0b2f646d4c1e00b7cf7bf5261f5aae
Sergey <sayvix005@gmail.com>
2021-06-22 15:56:39 +0300

    any commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = '';
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 let personalMovieDB = {
     count: numberOfFilms,
     movie: {},
@@ -8,10 +9,34 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt('Какой фильм вы смотрели последним?', ''),
-    b = prompt('Дайте оценку этому фильму:', ''),
-    c = prompt('Какой фильм вы смотрели последним?', ''),
-    d = prompt('Дайте оценку этому фильму:', '');
-    personalMovieDB.movie[a] = b;
-    personalMovieDB.movie[c] = d;
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов!");
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert("Вы классический зритель!");
+}
+else if (personalMovieDB.count > 30){
+    alert("Да вы киноман!");
+}
+else{
+    alert("Произошла ошибка!");
+}
+
+let a = '',
+    b = '';
+
+for( let i = 0; i < 2; i++){
+    let a = prompt('Какой фильм вы смотрели последним?', ''),
+        b = +prompt('Дайте оценку этому фильму:', '');
+    if(a != null && b != null && a != '' && b != '' && a.length <= 50){
+        personalMovieDB.movie[a] = b;
+        console.log("done");
+    }
+    else{
+        console.log("error");
+        i--;
+    }
+}
+console.log(personalMovieDB);
+
 console.log(personalMovieDB);
